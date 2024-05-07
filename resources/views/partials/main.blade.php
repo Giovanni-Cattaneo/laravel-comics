8bd04a397ac4a79032980813cb46be9fb43b9743
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-07 17:33:12 +0200

    add foreach in main, edit error in routes

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -13,6 +13,7 @@
             left: 20rem;
             color: white;
         }
+
     }
 </style>
 
@@ -25,16 +26,23 @@
         <h2 class="p-1 py-0">CURRENT SERIES</h2>
     </div>
 
-    <div class="comics">
-        @foreach ($products as $product)
-        <div class="card">
-            <img class="card-img-top" src="{{$product['thumb']}}" alt="Title" />
-            <div class="card-body">
-                <h4 class="card-title">{{$product['title']}}</h4>
-                <p class="card-text">Text</p>
+    <div class="container mt-5">
+        <div class="row">
+            @foreach ($products as $product)
+            <div class="col-4">
+                <div class="card">
+                    <img class="card-img-top" src="{{$product['thumb']}}" alt="Title" />
+                    <div class="card-body">
+                        <h4 class="card-title">{{$product['title']}}</h4>
+                        <p class="card-text">Text</p>
+                    </div>
+                </div>
+
             </div>
+            @endforeach
         </div>
 
-        @endforeach
+
+
     </div>
 </main>
--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/products', function () {
     $products = config('db.products');
-
-
-    return view('products');
-});
+    return view('home', compact('products'));
+})->name('home');
