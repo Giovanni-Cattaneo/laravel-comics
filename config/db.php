0e31acad3073742117b6fb77cfc2ee8e9c58581b
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-07 16:22:53 +0200

    add db for comics

---
+++ config/db.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'products' => [
+        [
+            "src" => "https://m.media-amazon.com/images/I/813JTCG1nwL._AC_UF1000,1000_QL80_.jpg",
+            "titolo" => "Action Comics",
+            "descrizione" => "Compra anche tu il nuovo fumetto di superman una grande esclusiva della dc comics"
+        ],
+        [
+            "src" => "https://m.media-amazon.com/images/I/813JTCG1nwL._AC_UF1000,1000_QL80_.jpg",
+            "titolo" => "Action Comics",
+            "descrizione" => "Compra anche tu il nuovo fumetto di superman una grande esclusiva della dc comics"
+        ],
+        [
+            "src" => "https://m.media-amazon.com/images/I/813JTCG1nwL._AC_UF1000,1000_QL80_.jpg",
+            "titolo" => "Action Comics",
+            "descrizione" => "Compra anche tu il nuovo fumetto di superman una grande esclusiva della dc comics"
+        ],
+        [
+            "src" => "https://m.media-amazon.com/images/I/813JTCG1nwL._AC_UF1000,1000_QL80_.jpg",
+            "titolo" => "Action Comics",
+            "descrizione" => "Compra anche tu il nuovo fumetto di superman una grande esclusiva della dc comics"
+        ],
+        [
+            "src" => "https://m.media-amazon.com/images/I/813JTCG1nwL._AC_UF1000,1000_QL80_.jpg",
+            "titolo" => "Action Comics",
+            "descrizione" => "Compra anche tu il nuovo fumetto di superman una grande esclusiva della dc comics"
+        ],
+        [
+            "src" => "https://m.media-amazon.com/images/I/813JTCG1nwL._AC_UF1000,1000_QL80_.jpg",
+            "titolo" => "Action Comics",
+            "descrizione" => "Compra anche tu il nuovo fumetto di superman una grande esclusiva della dc comics"
+        ],
+    ]
+];
--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -5,11 +5,25 @@
             width: 100%;
             height: 600px;
         }
+
+        .current {
+            background-color: #0282f9;
+            position: absolute;
+            top: 36.5rem;
+            left: 20rem;
+            color: white;
+        }
     }
 </style>
 
-<main>
+<main class="position-relative">
     <div class="jumbotron">
         <img src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="">
     </div>
+
+    <div class="current">
+        <h2 class="p-1 py-0">CURRENT SERIES</h2>
+    </div>
+
+    <div class="comics"></div>
 </main>
