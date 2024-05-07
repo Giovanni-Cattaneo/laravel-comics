04ab78b947613739dd8c47d5e58748aebcf44305
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-07 16:03:08 +0200

    add jumbotron and main partials

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -18,9 +18,7 @@
 <body>
     @include('partials.header')
 
-    <main>
-        @yield('main-content')
-    </main>
+    @include('partials.main')
 
     @include('partials.footer')
 
---
+++ resources/views/partials/main.blade.php
@@ -0,0 +1,15 @@
+<style>
+    main {
+
+        & img {
+            width: 100%;
+            height: 600px;
+        }
+    }
+</style>
+
+<main>
+    <div class="jumbotron">
+        <img src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="">
+    </div>
+</main>
